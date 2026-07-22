44bfdce0a775a6253b0d7bd35130c632f641603f
LatinMaestro <patrick.arteaga@hotmail.com>
2026-07-23 02:19:50 +1000

    Add PostgreSQL dataset acceptance executor

---
+++ database/migrations/0002_dataset_acceptance.down.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+DROP FUNCTION IF EXISTS dna.rollback_active_dataset(text, text, timestamptz);
+DROP FUNCTION IF EXISTS dna.accept_staged_dataset(
+  uuid,
+  uuid,
+  timestamptz,
+  timestamptz,
+  timestamptz
+);
+
+DROP TABLE IF EXISTS dna.dataset_record_contribution;
+DROP TABLE IF EXISTS dna.dataset_version_record;
+DROP TABLE IF EXISTS dna.dataset_staged_record;
+DROP TABLE IF EXISTS dna.dataset_stream;
+
+ALTER TABLE dna.dataset_version
+  DROP CONSTRAINT IF EXISTS dataset_version_owner_batch_unique;
+ALTER TABLE dna.dataset_version
+  DROP CONSTRAINT IF EXISTS dataset_version_owner_id_source_unique;
+ALTER TABLE dna.import_batch
+  DROP CONSTRAINT IF EXISTS import_batch_owner_id_source_unique;
+
+COMMIT;
