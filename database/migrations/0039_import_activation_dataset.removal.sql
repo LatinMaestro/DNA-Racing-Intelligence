db34ea54842d9316e8a53ef87b643811ea801b10
LatinMaestro <patrick.arteaga@hotmail.com>
2026-08-21 10:39:20 +1000

    Cover activation readiness migration lifecycle

--- database/migrations/0039_import_activation_dataset.removal.sql
+++ database/migrations/0039_import_activation_dataset.removal.sql
@@ -1,6 +1,11 @@
 DO $removal$
 BEGIN
   IF to_regprocedure(
+    'dna.assert_import_activation_ready(uuid,text,character)'
+  ) IS NOT NULL THEN
+    RAISE EXCEPTION 'activation readiness function still exists';
+  END IF;
+  IF to_regprocedure(
     'dna.prepare_import_activation_dataset(uuid,uuid,uuid,character,integer)'
   ) IS NOT NULL THEN
     RAISE EXCEPTION 'import activation dataset preparation function still exists';
